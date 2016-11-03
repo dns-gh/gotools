930f8a43f2d262fce13c867c26df51a35835ccdf
Dimitri <dns@masagroup.net>
2016-11-03 16:33:19 +0100

    [gotrace] add simple gotrace package to handle easily asserts and checks among go tests

--- src/compress/compress/compress_test.go
+++ src/compress/compress/compress_test.go
@@ -1,12 +1,9 @@
 package compress
 
 import (
-	"crypto/rand"
-	"encoding/binary"
+	"gotrace"
 	"os"
 	"path/filepath"
-	"runtime"
-	"strconv"
 	"testing"
 )
 
@@ -17,50 +14,18 @@ const (
 	testData      = "this is a test"
 )
 
-func traceBack(t *testing.T, err error) {
-	info := ""
-	if err != nil {
-		info = err.Error()
-	}
-	t.Error("Test failed ", info)
-	runtime.Goexit()
-}
-
-func assert(t *testing.T, err error) {
-	if err != nil {
-		traceBack(t, err)
-	}
-}
-
-func check(t *testing.T, condition bool) {
-	if !condition {
-		traceBack(t, nil)
-	}
-}
-
-// makeUniqueFolder creates a folder with pseudo
-// random generator
-func makeUniqueFolder(t *testing.T) string {
-	bytes := make([]byte, 8)
-	_, err := rand.Read(bytes)
-	if err != nil {
-		t.Error("Test failed:", err.Error())
-	}
-	return strconv.FormatUint(binary.BigEndian.Uint64(bytes), 10)
-}
-
 func makeFiles(t *testing.T) string {
-	unique := makeUniqueFolder(t)
+	unique := gotrace.MakeUniqueFolder(t)
 	root := filepath.Join(currentFolder, unique)
-	sub := filepath.Join(currentFolder, unique, makeUniqueFolder(t))
+	sub := filepath.Join(currentFolder, unique, gotrace.MakeUniqueFolder(t))
 	os.MkdirAll(sub, 0777)
 	file1, err := os.Create(filepath.Join(root, fileTest1))
-	assert(t, err)
+	gotrace.Assert(t, err)
 	defer file1.Close()
 	file2, err := os.Create(filepath.Join(sub, fileTest2))
-	assert(t, err)
+	gotrace.Assert(t, err)
 	_, err = file2.WriteString(testData)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	defer file2.Close()
 	return root
 }
@@ -68,65 +33,55 @@ func makeFiles(t *testing.T) string {
 func makeZip(t *testing.T) string {
 	root := makeFiles(t)
 	zipFile, err := Zip(root)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	info, err := os.Stat(zipFile)
-	assert(t, err)
-	check(t, info.Name() == zipFile)
-	check(t, info.Size() > 0)
+	gotrace.Assert(t, err)
+	gotrace.Check(t, info.Name() == zipFile)
+	gotrace.Check(t, info.Size() > 0)
 	err = os.RemoveAll(root)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	return zipFile
 }
 
 func TestZip(t *testing.T) {
 	zipFile := makeZip(t)
 	err := os.RemoveAll(zipFile)
-	assert(t, err)
-}
-
-func checkContent(t *testing.T, path, expected string) {
-	file, err := os.Open(path)
-	assert(t, err)
-	defer file.Close()
-	bytes := make([]byte, len(expected))
-	_, err = file.Read(bytes)
-	assert(t, err)
-	check(t, string(bytes) == expected)
+	gotrace.Assert(t, err)
 }
 
 func TestUnzip(t *testing.T) {
 	zipFile := makeZip(t)
 	dst, err := Unzip(zipFile)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	dst1, err := Unzip(zipFile)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	dst2, err := Unzip(zipFile)
-	assert(t, err)
-	check(t, dst1 == dst+".0")
-	check(t, dst2 == dst+".1")
+	gotrace.Assert(t, err)
+	gotrace.Check(t, dst1 == dst+".0")
+	gotrace.Check(t, dst2 == dst+".1")
 	err = os.RemoveAll(dst1)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	err = os.RemoveAll(dst2)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	num := 0
 	found := 0
 	filepath.Walk(dst, func(path string, info os.FileInfo, err error) error {
-		num += 1
+		num++
 		base := filepath.Base(path)
 		if base == fileTest1 {
-			found += 1
-			checkContent(t, path, "")
+			found++
+			gotrace.CheckContent(t, path, "")
 		}
 		if base == fileTest2 {
-			found += 1
-			checkContent(t, path, testData)
+			found++
+			gotrace.CheckContent(t, path, testData)
 		}
 		return nil
 	})
-	check(t, num == 4)
-	check(t, found == 2)
+	gotrace.Check(t, num == 4)
+	gotrace.Check(t, found == 2)
 	err = os.RemoveAll(zipFile)
-	assert(t, err)
+	gotrace.Assert(t, err)
 	err = os.RemoveAll(dst)
-	assert(t, err)
+	gotrace.Assert(t, err)
 }
---
+++ src/gotrace/gotrace.go
@@ -0,0 +1,80 @@
+package gotrace
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"testing"
+)
+
+const (
+	testDir = "test"
+)
+
+func traceBack(t *testing.T, err error) {
+	info := ""
+	if err != nil {
+		info = err.Error()
+	}
+	t.Error("Test failed ", info)
+	runtime.Goexit()
+}
+
+// Assert makes the test to fail and dumps
+// the trace if error is not nil
+func Assert(t *testing.T, err error) {
+	if err != nil {
+		traceBack(t, err)
+	}
+}
+
+// Check checks that the condition is verified.
+// It makes the test to fail and dumps the trace
+// as well otherwise.
+func Check(t *testing.T, condition bool) {
+	if !condition {
+		traceBack(t, nil)
+	}
+}
+
+// CheckContent checks that the input file contains
+// the expected information.
+// It makes the test to fail and dumps the trace
+// as well otherwise.
+func CheckContent(t *testing.T, path, expected string) {
+	file, err := os.Open(path)
+	Assert(t, err)
+	defer file.Close()
+	bytes := make([]byte, len(expected))
+	_, err = file.Read(bytes)
+	Assert(t, err)
+	Check(t, string(bytes) == expected)
+}
+
+// MakeUniqueFolder creates a folder with pseudo
+// random generator
+func MakeUniqueFolder(t *testing.T) string {
+	bytes := make([]byte, 8)
+	_, err := rand.Read(bytes)
+	Assert(t, err)
+	return strconv.FormatUint(binary.BigEndian.Uint64(bytes), 10)
+}
+
+// MakeUniqueTestFolder creates a unique test folder inside
+// the pre-defined test folder.
+func MakeUniqueTestFolder(t *testing.T) string {
+	return filepath.Join(testDir, MakeUniqueFolder(t))
+}
+
+// GetTestFolder returns the hardcoded test folder
+func GetTestFolder() string {
+	return testDir
+}
+
+// RemoveTestFolder removes the hardcoded test folder
+func RemoveTestFolder(t *testing.T) {
+	Assert(t, os.RemoveAll(GetTestFolder()))
+}
